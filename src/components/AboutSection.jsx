2e725aa5770f18a49dae277ee6a7df4bd4db8ad4
RustedSwords <manavmakwana27@gmail.com>
2026-04-30 14:49:48 +0200

    update resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -66,7 +66,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://www.overleaf.com/read/rwpdfmxgjypt#73fcc0"
+                href="https://www.overleaf.com/read/jmbvfmzzctdk#b40456"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
